b8b445a5190b144d8cd1f1ab929bfad6e1cb8422
Avi Craimer <mailavi@gmail.com>
2025-07-23 15:15:53 +0100

    Rendered notecard

--- new/src/app.ts
+++ new/src/app.ts
@@ -2,11 +2,12 @@
 import { DrawingModel } from "./DrawingData/DrawingModel";
 import { initListeners, initDraw, renderLoop } from "./canvas/canvas";
 import { initControlListeners } from "./controls";
+import { initNotecard } from "./matt/notecard";
 import { initPalletButtons } from "./pallet/initPallet";
 
 function initApp() {
     const model = new DrawingModel();
-
+    initNotecard();
     initDraw(model).then((draw) => {
         initListeners(draw);
         renderLoop(draw);
